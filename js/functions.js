c9f745fc10b9fa0e672c55e41e0dee1f4270c045
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-03-23 20:46:15 -0300

    Animação no scrool e estilização na barra de rolagem

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+$('.navbar-nav a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+  targetOffset = $(id).offset().top;
+
+  $('html, body').animate({
+    scrollTop: targetOffset - 70
+
+  }, 150);
+})
